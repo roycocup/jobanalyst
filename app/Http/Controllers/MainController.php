dcddd29dd44f574091e3073e26d05b61148665cc
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-05-09 21:18:15 +0100

    including lumen

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+
+    public function test(Request $request)
+    {
+        return "hey";
+    }
+}
